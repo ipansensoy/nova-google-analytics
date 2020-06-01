7facc69f03a0ea3f8ad45e7b8db196404d7fb055
Ian Pansensoy <ian.pansensoy@news.com.au>
2020-06-01 12:20:16 +0800

    added tag loaded card

--- resources/js/card.js
+++ resources/js/card.js
@@ -1,4 +1,5 @@
 Nova.booting((Vue, router) => {
-    Vue.component('most-visited-pages', require('./components/MostVisitedPages'));
-    Vue.component('referrer-list', require('./components/ReferrerList'));
-})
+    Vue.component("most-visited-pages", require("./components/MostVisitedPages"));
+    Vue.component("referrer-list", require("./components/ReferrerList"));
+    Vue.component("tag-loaded", require("./components/TagLoaded"));
+});
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Tightenco\NovaGoogleAnalytics\Http\Controllers\MostVisitedPagesController;
 use Tightenco\NovaGoogleAnalytics\Http\Controllers\ReferrerListController;
+use Tightenco\NovaGoogleAnalytics\Http\Controllers\TagLoadedController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +14,8 @@ use Tightenco\NovaGoogleAnalytics\Http\Controllers\ReferrerListController;
 | are loaded by the ServiceProvider of your tool. They are protected
 | by your tool's "Authorize" middleware by default. Now, go build!
 |
-*/
+ */
 
 Route::get('most-visited-pages', MostVisitedPagesController::class);
 Route::get('referrer-list', ReferrerListController::class);
+Route::get('tag-loaded', TagLoadedController::class);
