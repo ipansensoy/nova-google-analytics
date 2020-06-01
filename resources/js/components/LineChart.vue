79187cc5b09ee675bcfaa185da94c9031ede4226
Ian Pansensoy <ian.pansensoy@news.com.au>
2020-06-01 18:43:45 +0800

    update card.js

--- resources/js/components/LineChart.vue
+++ resources/js/components/LineChart.vue
@@ -21,8 +21,7 @@ export default {
 
         for (let i = 0; i < this.data.length; i++) {
             chartData.labels.push(moment(String(this.data[i].date)).format("LL"));
-            chartData.datasets.push({ label: this.data[i].name });
-
+            chartData.labels.push(this.data[i].name);
             chartData.datasets[0].data.push(this.data[i].count);
         }
 
