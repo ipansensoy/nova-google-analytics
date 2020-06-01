a901375b6b6ea1aa6fcc64c3d939401b5ae5b8e1
Ian Pansensoy <ian.pansensoy@news.com.au>
2020-06-01 12:20:59 +0800

    added tag loaded card

---
+++ resources/js/components/TagLoaded.vue
@@ -0,0 +1,42 @@
+<template>
+    <card class="px-4 py-4">
+        <div class="mb-4">
+            <h3 class="mr-3 text-base text-80 font-bold">Tag Load Count</h3>
+        </div>
+        <div v-if="!pages" class="flex items-center">
+            <p class="text-80 font-bold">No Data</p>
+        </div>
+        <ul v-else class="most-visited-pages-list mb-4 mt-2 overflow-y-scroll">
+            <li v-for="page in pages">
+                <a :href="`https://${page.hostname}${page.path}`" target="_blank">{{ page.name }}</a
+                >: {{ page.count }}
+            </li>
+        </ul>
+    </card>
+</template>
+
+<script>
+export default {
+    props: ["card"],
+
+    data: function() {
+        return {
+            pages: []
+        };
+    },
+
+    mounted() {
+        Nova.request()
+            .get("/nova-vendor/nova-google-analytics/tag-loaded")
+            .then(response => {
+                this.pages = response.data;
+            });
+    }
+};
+</script>
+
+<style scoped>
+.most-visited-pages-list {
+    height: 4.6rem;
+}
+</style>
---
+++ src/Http/Controllers/TagLoadedController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tightenco\NovaGoogleAnalytics\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use Spatie\Analytics\Analytics;
+use Spatie\Analytics\Period;
+
+class TagLoadedController extends Controller
+{
+    public function __invoke()
+    {
+        return $this->tagLoaded();
+    }
+
+    private function tagLoaded()
+    {
+        $analyticsData = app(Analytics::class)->performQuery(
+            Period::days(7),
+            'ga:users',
+            [
+                'metrics' => 'ga:Tag Load Count',
+                'dimensions' => 'ga:pageTitle,ga:hostname,ga:pagePath',
+                'sort' => '-ga:Tag Load Count',
+                'max-results' => 10,
+            ]
+        );
+
+        $headers = ['name', 'hostname', 'path', 'count'];
+
+        return array_map(
+            function ($row) use ($headers) {
+                return array_combine($headers, $row);
+            },
+            $analyticsData->rows ?? []
+        );
+    }
+}
