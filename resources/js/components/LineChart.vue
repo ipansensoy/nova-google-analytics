59e5ab0488fd784792201cda4ca6690b9f6e56d3
Ian Pansensoy <ian.pansensoy@news.com.au>
2020-06-01 19:09:50 +0800

    update card.js

--- resources/js/components/LineChart.vue
+++ resources/js/components/LineChart.vue
@@ -12,26 +12,26 @@ export default {
             labels: [],
             datasets: [
                 {
-                    label: "Data One",
+                    label: "1Count",
                     backgroundColor: "#f87979",
-                    data: [1, 2]
+                    data: [70, 100, 400, 180, 100, 300, 500]
                 },
                 {
-                    label: "Data two",
-                    backgroundColor: "#f87979",
-                    data: [3, 4]
+                    label: "2Count",
+                    backgroundColor: "#f83979",
+                    data: [10, 30, 700, 180, 100, 300, 500]
                 },
                 {
-                    label: "Data three",
-                    backgroundColor: "#f87979",
-                    data: [5, 6]
+                    label: "3Count",
+                    backgroundColor: "#f39719",
+                    data: [70, 100, 40, 180, 100, 300, 200]
                 }
             ]
         };
 
         for (let i = 0; i < this.data.length; i++) {
             chartData.labels.push(moment(String(this.data[i].date)).format("LL"));
-            chartData.datasets[i].data.push(this.data[i].count);
+            //chartData.datasets[0].data.push(this.data[i].count);
         }
 
         this.renderChart(chartData, {
