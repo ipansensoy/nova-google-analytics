6dc475374b6205aa4b3442d50d257b7a7fa0b31a
Ian Pansensoy <ian.pansensoy@news.com.au>
2020-06-01 17:50:47 +0800

    update card.js

--- resources/js/components/LineChart.vue
+++ resources/js/components/LineChart.vue
@@ -19,7 +19,7 @@ export default {
         };
 
         for (let i = 0; i < this.data.length; i++) {
-            chartData.labels.push(this.data[i].name);
+            chartData.labels.push(new Date(this.data[i].date).toDateString());
             chartData.datasets[0].data.push(this.data[i].count);
         }
 
