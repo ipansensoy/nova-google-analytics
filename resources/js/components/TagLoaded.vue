144dbd46536bac401f4b20547cf382c9fa190608
Ian Pansensoy <ian.pansensoy@news.com.au>
2020-06-02 09:27:43 +0800

    updated vue component

--- resources/js/components/TagLoaded.vue
+++ resources/js/components/TagLoaded.vue
@@ -1,12 +1,14 @@
 <template>
     <card class="px-4 py-4 pie-char-panel">
-        <h3 class="mr-3 text-base text-80 font-bold">Tag Load Count</h3>
-        <PieChart v-if="loaded" :data="loadCount" :styles="styles" />
+        <h2>Views by Category</h2>
+        <apexchart v-if="loaded" width="400" type="pie" :options="catOptions" :series="catSeries"></apexchart>
     </card>
 </template>
 
 <script>
-import PieChart from "./PieChart";
+import VueApexCharts from "vue-apexcharts";
+Vue.use(VueApexCharts);
+Vue.component("apexchart", VueApexCharts);
 export default {
     name: "App",
     props: ["card"],
@@ -16,18 +18,31 @@ export default {
     data() {
         return {
             loaded: false,
-            loadCount: null,
-            styles: {
-                width: "450px",
-                height: "400px",
-                position: "relative"
-            }
+            catOptions: null,
+            catSeries: null,
+            visitorOptions: null,
+            visitorSeries: null
         };
     },
     async created() {
         let resp = await fetch("/nova-vendor/nova-google-analytics/tag-loaded");
         let data = await resp.json();
-        this.loadCount = data;
+        let countByHost = data.map(item => {
+            let obj = {};
+            obj[item.hostname] = parseInt(item.count);
+            return obj;
+        });
+        this.catOptions = {
+            labels: []
+        };
+
+        this.catSeries = [];
+
+        for (let cat in countByHost) {
+            console.log(cat);
+            this.catOptions.labels.push(cat);
+            this.catSeries.push(countByHost[cat]);
+        }
         this.loaded = true;
     }
 };
