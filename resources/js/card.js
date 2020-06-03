725af7ac3d5d313c704649fdb9886bd2c72a74a2
Ian Pansensoy <ian.pansensoy@news.com.au>
2020-06-03 13:59:09 +0800

    added media type card

--- resources/js/card.js
+++ resources/js/card.js
@@ -5,4 +5,5 @@ Nova.booting((Vue, router) => {
     Vue.component("most-visited-pages", require("./components/MostVisitedPages"));
     Vue.component("referrer-list", require("./components/ReferrerList"));
     Vue.component("tag-loaded", require("./components/TagLoaded"));
+    Vue.component("media-type-loaded", require("./components/MediaTypeLoaded"));
 });
