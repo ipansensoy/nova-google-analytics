03d036250801af6a1fe65e55467fcd5e72b948a6
Ian Pansensoy <ian.pansensoy@news.com.au>
2020-06-04 15:31:22 +0800

    update vide ad data structure

--- src/Http/Controllers/PrebidRequestController.php
+++ src/Http/Controllers/PrebidRequestController.php
@@ -20,14 +20,15 @@ class PrebidRequest extends Controller
             Period::days(7),
             'ga:users',
             [
-                'metrics' => 'ga:metric10,ga:metric11',
-                'dimensions' => 'ga:hostname',
-                'sort' => '-ga:metric10',
+                'metrics' => 'ga:totalEvents,ga:eventValue,ga:avgEventValue',
+                'dimensions' => 'ga:hostname,ga:eventCategory,ga:eventAction,ga:eventLabel',
+                'sort' => '-ga:hostname,-ga:eventCategory,-ga:eventAction,-ga:eventLabel',
+                'filter' => 'ga:eventCategory=~^Prebid.js;ga:eventAction=~Request',
                 'max-results' => 10,
             ]
         );
 
-        $headers = ['hostname', 'video_count', 'banner_count'];
+        $headers = ['hostname', 'category', 'action', 'label', 'total', 'value', 'avg'];
 
         return array_map(
             function ($row) use ($headers) {
